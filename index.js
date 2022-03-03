3b8baff1eb6f744a7b6a987e5e0a3e0b0a03bdcc
AndyFang36 <andylovesapple666@icloud.com>
2022-03-03 19:23:14 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+/**
+ * Making document return to top when refreshing the current page.
+ * @author <a mailto:andylovesapple666@icloud.com>Andy Fang</a>
+ */
+export const refreshToTop = () => {
+    window.addEventListener("beforeunload", () => {
+        document.documentElement.scrollTop = 0;
+        document.body.scrollTop = 0;
+    });
+}
