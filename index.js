a2c58221bc2a78b75e38eaa40bdaaf1d20c3c7eb
AndyFang36 <andylovesapple666@icloud.com>
2022-03-05 16:07:28 +0800

    update info

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 /**
- * Making document return to top when refreshing the current page.
+ * @name refreshToTop
+ * @description Making document return to top when refreshing the current page.
  * @author <a href=mailto:andylovesapple666@icloud.com>Andy Fang</a>
+ * @version 1.0.0
+ * @licence MPL-2.0
  */
 export const refreshToTop = () => {
     window.addEventListener("beforeunload", () => {
