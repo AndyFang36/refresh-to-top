c15742b7a1dd6655e2a86c574b114b144288a1a4
AndyFang36 <andylovesapple666@icloud.com>
2022-03-05 17:24:10 +0800

    optimization

--- index.js
+++ index.js
@@ -2,12 +2,11 @@
  * @name refreshToTop
  * @description Making document return to top when refreshing the current page.
  * @author <a href=mailto:andylovesapple666@icloud.com>Andy Fang</a>
- * @version 1.0.0
+ * @version 1.1.0
  * @licence MPL-2.0
  */
 export const refreshToTop = () => {
-    window.addEventListener("beforeunload", () => {
-        document.documentElement.scrollTop = 0;
-        document.body.scrollTop = 0;
+    window.addEventListener("unload", () => {
+        window.scrollTo(0, 0);
     });
 }
