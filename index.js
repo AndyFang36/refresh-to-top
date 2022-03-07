5edfca3224715184aa8e1692f4da1317165a6bb9
AndyFang36 <andylovesapple666@icloud.com>
2022-03-07 16:44:48 +0800

    .npmignore file

--- index.js
+++ index.js
@@ -2,11 +2,11 @@
  * @name refreshToTop
  * @description Making document return to top when refreshing the current page.
  * @author <a href=mailto:andylovesapple666@icloud.com>Andy Fang</a>
- * @version 1.1.0
+ * @version 1.1.1
  * @licence MPL-2.0
  */
 export const refreshToTop = () => {
     window.addEventListener("unload", () => {
-        window.scrollTo(0, 0);
+        scrollTo(0,0);
     });
 }
